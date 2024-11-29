671c9a9c1e1ef44b2db63a1690ff22cd2258284d
Alisha Burgfeld <alisha.burgfeld@alishas-mac.mynetworksettings.com>
2024-11-29 15:26:56 -0500

    set initial exercise record, group, and set up an initial exercise.

--- back-end/src/main/java/com/allyoucanexercise/back_end/exercise/Exercise.java
+++ back-end/src/main/java/com/allyoucanexercise/back_end/exercise/Exercise.java
@@ -1,5 +1,6 @@
 package com.allyoucanexercise.back_end.exercise;
 
-public class Exercise {
+public record Exercise(Integer id, String name, Group group, String description) {
     
+    // eventually add equipment, but not sure what type it would be since something like pushups would be none, home gym, and full gym
 }
