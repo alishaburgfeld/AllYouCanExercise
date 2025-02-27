34636690741f1427b9681efb09d34b97866c5c1f
Alisha Burgfeld <alisha.burgfeld@mac.mynetworksettings.com>
2025-02-27 17:07:20 -0500

    setup jest and babel for tests. also added transform ignore so jest ignores images and css imports

--- front-end/src/tests/App.test.js
+++ front-end/src/tests/App.test.js
@@ -1,12 +1,12 @@
 import { BrowserRouter } from "react-router";
 import { render, screen } from "@testing-library/react";
-import App from "../../../front-end-old/src/App";
+import App from "../../../front-end/src/App";
 
 test("renders Navbar text", () => {
   render(
     <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter>,
   );
   const navbarText = screen.getByText(/All You Can Exercise/i);
   expect(navbarText).toBeInTheDocument();
