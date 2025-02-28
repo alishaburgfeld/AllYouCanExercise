5fa528f852a69dba1ef5f0af9e87910061fe5fe8
Alisha Burgfeld <alisha.burgfeld@mac.mynetworksettings.com>
2025-02-28 08:25:30 -0500

    added more esm support, fixed my preset issue, used jest transform stub, and added support for importing meta. Tests appear to be running.

--- front-end/src/tests/App.test.js
+++ front-end/src/tests/App.test.js
@@ -3,15 +3,8 @@ import { render, screen } from "@testing-library/react";
 import App from "../../../front-end/src/App";
 
 test("renders Navbar text", () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
+  render(<App />);
   const navbarText = screen.getByText(/All You Can Exercise/i);
   expect(navbarText).toBeInTheDocument();
   expect(screen.getByRole("navigation")).toBeInTheDocument();
 });
-
-// https://www.npmjs.com/package/react-router-dom
-// https://stackoverflow.com/questions/79256904/cannot-detect-installed-react-router-dom-package-when-running-unit-tests
